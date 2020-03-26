097825273ef2b379325724f22091bdabe3fb19a1
MrRinkana <39345772+MrRinkana@users.noreply.github.com>
2020-03-26 23:07:33 +0100

    failed attempt at int unique id

--- Projekt-bankapp/src/Bank.java
+++ Projekt-bankapp/src/Bank.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 
 public class Bank {
 	ArrayList<BankAccount> accountList;
+	ArrayList<Customer> listOfCustomers; //Tror detta underlättar, alltså att bankaccount bara får pekare till objekt i denna
+	UniqueINT uniqID = new UniqueINT();
 	
 	public Bank() {
 		accountList = new ArrayList<BankAccount>();
@@ -22,6 +24,28 @@ public class Bank {
 		}
 		return null;
 	}
+	
+	/*
+	 * Dessa två funkar inte då de måste vara static, annars måste customer och 
+	 * bankaccount ha ett bank objekt, och det går ju inte. Skulle vilja bara skicka in deet genom construktooorn!
+	 * -m
+	 */
+	
+	
+	/*
+	 * Används av Customer för unikt användarnummer
+	 */
+	public int giveUserID() {
+		return uniqID.uniqueIntC(listOfCustomers);
+	}
+	
+	/*
+	 * Används av BankAccount för unikt kontonummer
+	 */
+	public int giveAccountID() {
+		return uniqID.uniqueIntB(accountList);
+	}
+	
 	
 	boolean removeAccount(int number) {
 		//detta är inkompatibelt med att använda index som kontonummer, måste hitta annan lösning.
