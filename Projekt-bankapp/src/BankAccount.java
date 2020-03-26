1b7b62d1fd32aa018006d1bd0c7a18bd8b0b7324
Fjantom <fjantom@gmail.com>
2020-03-26 21:08:58 +0000

    Måste manuellt lägga till ändringen genom github
    Commit genom eclipse ville bara se ändringar till bank.java :((((((

--- Projekt-bankapp/src/BankAccount.java
+++ Projekt-bankapp/src/BankAccount.java
@@ -1,8 +1,37 @@
 
 public class BankAccount {
-
+	private Customer holder;
+	private double cashMoney;
 	
-	//test
+	public BankAccount(String holderName, long holderId) {
+		this.holder = new Customer(holderName, holderId);
+		cashMoney = 0;
+	}
 	
+	public BankAccount(Customer holder) {
+		this.holder = holder;
+	}
 	
+	public Customer getHolder() {
+		return holder;
+	}
+	/*
+	public int getAccountNumber() {
+		return holder.getCustomerNr();
+	}
+	*/
+	public double getAmount() {
+		return cashMoney;
+	}
+	
+	void deposit(double amount) {
+		cashMoney += amount;
+	}
+	void withdraw(double amount) {
+		cashMoney -= amount;
+	}
+	
+	public String toString() {
+		return ("Kontoägare: " + holder.toString() + "Cash money: " + getAmount());
+	}
 }
