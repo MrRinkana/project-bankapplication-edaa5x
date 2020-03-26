25985d60b50e1af023e83d025a6f57a94f4f42aa
MrRinkana <39345772+MrRinkana@users.noreply.github.com>
2020-03-26 22:48:44 +0100

    Customer upd. and initial UniqueINT

--- Projekt-bankapp/src/Customer.java
+++ Projekt-bankapp/src/Customer.java
@@ -1,21 +1,9 @@
-import java.math.BigInteger;
 
 public class Customer {
 
 	private String name;
 	private long idNr;
-	private BigInteger custNbr; //long too short
-	/*
-	 * Insåg senare att det var nog meningen det unika customer id skulle vara 
-	 * typ 1, 2, 3 osv (bokstavligen kund nummer: x).
-	 * Eftersom denna klass inte har och ej bör ha tillgång till andra objekt av samma typ
-	 * så kan ett sådant id endast genereras utanför i en annan klass som har rätt att räkna
-	 * objekt eller håller koll på senast skapade konto-nummret. Tycker ett sådant system 
-	 * är lite fult. Min lösning är även thread-safe ;)
-	 * 
-	 * Måste custNbr vara ev typen long som i ledningen så är det bara att byta till
-	 * longGenerator i UniqueIDGen
-	 */
+	private int custNbr;
 	
 
 	/*
@@ -24,13 +12,13 @@ public class Customer {
 	public Customer(String name, long idNr) {
 		this.name = name;
 		this.idNr = idNr;
-		this.custNbr = UniqueIDGen.generator((byte) 1); //1 representerar typen "customer", se UniqueIDGen för mer info
+		this.custNbr = 666;
 	}
 
 	/*
 	 * Laddar in en befintlig användare. (för framtida inläsning från fil tänkte jag)
 	 */
-	public Customer(String name, long idNr, BigInteger custNbr) {
+	public Customer(String name, long idNr, int custNbr) {
 		this.name = name;
 		this.idNr = idNr;
 		this.custNbr = custNbr;
@@ -53,12 +41,8 @@ public class Customer {
 	/*
 	 * Svarar med kundens unika ID
 	 */
-	public BigInteger getCustomerNr() {
+	public int getCustomerNr() {
 		return custNbr;
-	}
-	
-	public String getCustomerNrAsString() {
-		return custNbr.toString();
 	}
 	
 	/*
