978772599bef730cb73a08ef138c8184bfc223b1
fjalar <fjantom@gmail.com>
2020-03-26 22:04:06 +0100

    Ground work för bank.java

---
+++ Projekt-bankapp/src/Bank.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Bank {
+	ArrayList<BankAccount> accountList;
+	
+	public Bank() {
+		accountList = new ArrayList<BankAccount>();
+	}
+	
+	public int addAccount(String holderName, long idNr) {
+		//TODO: söka igenom så att man inte får redundant account
+		BankAccount account = new BankAccount(holderName, idNr);
+		accountList.add(account);
+		return(accountList.indexOf(account) + 1);
+	}
+	
+	public Customer findHolder(long idNr) {
+		for (BankAccount c : accountList) {
+			if (c.getHolder().getIdNr() == idNr) {
+				return(c.getHolder());
+			}
+		}
+		return null;
+	}
+	
+	boolean removeAccount(int number) {
+		//detta är inkompatibelt med att använda index som kontonummer, måste hitta annan lösning.
+		}
+}
