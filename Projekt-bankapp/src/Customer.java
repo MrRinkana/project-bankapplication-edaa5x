1ecae7165565a325db2a8d3c5db05e38d3d85e1d
MrRinkana <39345772+MrRinkana@users.noreply.github.com>
2020-03-27 10:04:02 +0100

    customer unique number implemented

--- Projekt-bankapp/src/Customer.java
+++ Projekt-bankapp/src/Customer.java
@@ -4,6 +4,7 @@ public class Customer {
 	private String name;
 	private long idNr;
 	private int custNbr;
+	static int totalCustomers = 0;
 	
 
 	/*
@@ -12,7 +13,8 @@ public class Customer {
 	public Customer(String name, long idNr) {
 		this.name = name;
 		this.idNr = idNr;
-		this.custNbr = 666;//Bank.giveUserID();  //Kan bank vara static, alltså det kommer vid ett visst tilfälle bara finnas en bank?
+		totalCustomers++;
+		this.custNbr = totalCustomers;
 	}
 
 	/*
@@ -39,7 +41,7 @@ public class Customer {
 	}
 	
 	/*
-	 * Svarar med kundens unika ID
+	 * Svarar med kundens unika (interna) nummer
 	 */
 	public int getCustomerNr() {
 		return custNbr;
